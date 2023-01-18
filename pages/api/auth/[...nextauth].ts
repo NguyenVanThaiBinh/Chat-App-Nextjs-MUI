79240b1baf02d91cc9a1222879f2b76dd4cbe476
Binh Hu <nguyenvanthaibinh2210@gmail.com>
2023-01-18 21:54:50 +0700

    open secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,7 +8,7 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  // secret: process.env.JWT_SECRET as string,
+  secret: process.env.JWT_SECRET as string,
   pages: {
     signIn: "/login",
   },
