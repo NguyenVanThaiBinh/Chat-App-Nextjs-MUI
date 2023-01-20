683b8f038246f0a1d3a5540bc7899768a86e4f99
Binh Hu <nguyenvanthaibinh2210@gmail.com>
2023-01-20 15:28:43 +0700

    update NEXTAUTH_URL

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import Body from "./Body";
 import React from "react";
 
-const dev = process.env.NODE_ENV != "production";
+const dev = process.env.NODE_ENV == "production";
 
 export const server = dev
   ? "http://localhost:3000"
